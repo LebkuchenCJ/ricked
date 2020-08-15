b0a661d9e833c8d63f3120b7eaa133cf7d3fb002
JonasImm <jonas.imm@gmail.com>
2020-08-15 11:46:04 +0200

    Adapted loading screen css and added new img

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Loading.css";
 import portalSrc from "../../assets/portal.gif";
+import mortySrc from "../../assets/morty.png";
 
 function LoadingScreen() {
   return (
     <div className="portalScreen">
       <img src={portalSrc} alt="Loading Screen" />
+      <img src={mortySrc} alt="Loading Screen" />
       <span>Loading...</span>
     </div>
   );
