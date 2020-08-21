4ab4bd395d968eeee334901726f1efe9b4608f40
JonasImm <jonas.imm@gmail.com>
2020-08-21 15:22:23 +0200

    Restyled link components

--- src/components/main/Planets.js
+++ src/components/main/Planets.js
@@ -3,9 +3,9 @@ import planetSrc from "../../assets/saturn.svg";
 
 export default function Planets(href, alt) {
   return (
-    <a href={href} alt={alt}>
+    <>
       <img src={planetSrc} alt="Planet Icon" />
       <span>Planets</span>
-    </a>
+    </>
   );
 }
