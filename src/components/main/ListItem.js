34f7bac9c55a2ffce9c0a893bf24cd4d47e36843
JonasImm <jonas.imm@gmail.com>
2020-08-21 10:21:33 +0200

    Added JSS to listitem and deleted listitem.css

--- src/components/main/ListItem.js
+++ src/components/main/ListItem.js
@@ -1,10 +1,38 @@
 import React from "react";
-import "./ListItem.css";
+import styled from "@emotion/styled";
+
+const Container = styled.a`
+  display: flex;
+  border-bottom: 1px solid #e4e4e4;
+  text-decoration: none;
+  align-items: center;
+  margin: 0 5px;
+
+  > img {
+    width: 60px;
+    height: 60px;
+    margin-right: 5px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+  span {
+    font-weight: bolder;
+  }
+  > p {
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+  }
+  > h4 {
+    flex-grow: 2;
+  }
+`;
 
 export default function ListItem({ href, children }) {
-  return (
-    <a href={href} className="listItem">
-      {children}
-    </a>
-  );
+  return <Container href={href}>{children}</Container>;
 }
