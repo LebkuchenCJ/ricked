227369a63eeef8eaa414e7c891874833e65ab9b9
JonasImm <jonas.imm@gmail.com>
2020-08-21 19:26:40 +0200

    Added character section to footer menu

--- src/components/main/AppFooter.js
+++ src/components/main/AppFooter.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 import Menu from "./Menu";
 import Planets from "./Planets";
+import Characters from "./Characters";
 import Episodes from "./Episodes";
 import Home from "./Home";
 import { Link } from "react-router-dom";
@@ -19,6 +20,9 @@ function AppFooter() {
       <Menu>
         <Link to="/">
           <Home />
+        </Link>
+        <Link to="/characters">
+          <Characters />
         </Link>
         <Link to="/planets">
           <Planets />
