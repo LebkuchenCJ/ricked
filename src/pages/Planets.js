8259b448fee53fa18801933c0c0ffd1b49a264e9
JonasImm <jonas.imm@gmail.com>
2020-08-21 18:39:21 +0200

    Connect query select and implement to planets.js, incl. some styling

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -5,60 +5,51 @@ import AppMain from "../components/main/AppMain";
 import List from "../components/main/List";
 import ListItem from "../components/main/ListItem";
 import ListItemImg from "../components/main/ListItemImg";
-import ListItemText from "../components/main/ListItemText";
 import ListItemType from "../components/main/ListItemType";
 import { fetchLocations, fetchCharacterName } from "../api/rickedApi";
 import LoadingScreen from "../components/loading/Loading";
 import ListItemDimension from "../components/main/ListItemDimension";
+import { useQuery } from "react-query";
+import PlanetList from "../components/main/PlanetList";
 
 function Planets(props) {
-  const [locations, setLocation] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [query, setQuery] = useState("");
+  //const [locations, setLocation] = useState(null);
+  //const [query, setQuery] = useState("");
+  //const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      async function fetchData() {
-        setLoading(false);
-        const locationIndex = await fetchLocations();
-        setLocation(locationIndex);
-        setLoading(true);
-      }
-      fetchData();
-    }, 2000);
-  }, []);
+  const { data, status } = useQuery("planets", fetchLocations);
 
-  let timeOutId;
+  console.log(data);
 
-  function handleChange(input) {
+  //let timeOutId;
+
+  /*  function handleChange(input) {
     setQuery(input);
     clearTimeout(timeOutId);
     timeOutId = setTimeout(async () => {
       const results = await fetchCharacterName(query);
       setLocation(results);
     }, 300);
-  }
-  if (loading === false) {
-    return <LoadingScreen />;
-  }
+  } */
+
   return (
     <>
       <AppHeader>
         <h1>Planets</h1>
-        <Input value={query} handleChange={(value) => handleChange(value)} />
+        <Input /* value={query} handleChange={(value) => handleChange(value)} */
+        />
       </AppHeader>
       <AppMain>
         <List>
-          {locations?.map((location) => (
-            <ListItem href={location.href} key={location.id}>
-              <ListItemImg />
-              <ListItemDimension
-                primary={location.name}
-                secondary={location.planet}
-              />
-              <ListItemType type={location.dimension} />
-            </ListItem>
-          ))}
+          {status === "loading" && <LoadingScreen />}
+          {status === "error" && <div>Error</div>}
+          {status === "success" && (
+            <>
+              {data.map((location) => (
+                <PlanetList key={location.name} planet={location} />
+              ))}
+            </>
+          )}
         </List>
       </AppMain>
     </>
