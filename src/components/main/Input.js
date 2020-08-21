4525357cb07333ac914eaf48780deadaa7a69ea4
JonasImm <jonas.imm@gmail.com>
2020-08-21 10:27:44 +0200

    Add JSS to input.js

--- src/components/main/Input.js
+++ src/components/main/Input.js
@@ -1,21 +1,49 @@
 import React from "react";
-import "./Input.css";
 import searchIconSrc from "../../assets/search-24px.svg";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  background-color: #a2ce47;
+  border-radius: 50px;
+  display: flex;
+  justify-content: space-between;
+
+  /* @media only screen and (max-width: 600px) {
+  .searchElement {
+    width: 100%;
+  }
+} */
+
+  > button {
+    background: transparent;
+    border: none;
+    display: flex;
+    justify-content: center;
+  }
+
+  > input {
+    color: #000;
+    background-color: rgba(162, 206, 71, 0.2);
+    border-style: none;
+    margin-left: 5px;
+    width: 100%;
+  }
+`;
 
 function Input(/* { query, handleChange } */) {
   return (
-    <div className="searchElement">
+    <Container>
       <input
         /* value={query}
         onChange={(event) => handleChange(event.target.value)} */
-        className="searchElement__input"
+
         placeholder="Search"
         type="texts"
       />
       <button>
         <img src={searchIconSrc} alt="Search Icon" />
       </button>
-    </div>
+    </Container>
   );
 }
 
