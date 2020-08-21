12f9154cbb427e96b4c0b69447ebf7f08e160793
JonasImm <jonas.imm@gmail.com>
2020-08-21 10:37:13 +0200

    Add JSS to loading screen tag and delete loading.css

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,15 +1,47 @@
 import React from "react";
-import "./Loading.css";
 import portalSrc from "../../assets/portal.gif";
 import mortySrc from "../../assets/morty.png";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: "schwifty", sans-serif;
+  -webkit-text-stroke: 0.5px #000;
+  text-shadow: -2px 0 rgb(162, 206, 71), 0 2px rgb(162, 206, 71),
+    2px 0 rgb(162, 206, 71), 0 -2px rgb(162, 206, 71);
+  font-weight: bolder;
+  font-size: 2rem;
+  position: relative;
+
+  img:nth-of-type(2) {
+    animation: loadingImg 2s;
+    position: absolute;
+    width: 15%;
+  }
+
+  @keyframes loadingImg {
+    from {
+      width: 1vw;
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
 
 function LoadingScreen() {
   return (
-    <div className="portalScreen">
+    <Container>
       <img src={portalSrc} alt="Loading Screen" />
       <img src={mortySrc} alt="Loading Screen" />
       <span>Loading...</span>
-    </div>
+    </Container>
   );
 }
 
