621de93bb797f9daba6bdbcc67e3911a30bbecad
JonasImm <jonas.imm@gmail.com>
2020-08-21 19:05:05 +0200

    Added episode api and link to episode.js

--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -1,7 +1,42 @@
 import React from "react";
+import AppHeader from "../components/main/AppHeader";
+import InputPlanets from "../components/main/InputPlanets";
+import AppMain from "../components/main/AppMain";
+import List from "../components/main/List";
+import LoadingScreen from "../components/loading/Loading";
+import PlanetList from "../components/main/PlanetList";
+import { useQuery } from "react-query";
+import { fetchEpisodes } from "../api/rickedApi";
 
 function Episodes(props) {
-  return <div>Episodes</div>;
+  const { data, status } = useQuery("episodes", fetchEpisodes);
+
+  console.log(data);
+
+  return (
+    <>
+      <AppHeader>
+        <h1>Planets</h1>
+        <InputPlanets
+        /*  value={query}
+          handleChange={(value) => handleChange(value)} */
+        />
+      </AppHeader>
+      <AppMain>
+        <List>
+          {status === "loading" && <LoadingScreen />}
+          {status === "error" && <div>Error</div>}
+          {status === "success" && (
+            <>
+              {data.map((location) => (
+                <PlanetList key={location.name} planet={location} />
+              ))}
+            </>
+          )}
+        </List>
+      </AppMain>
+    </>
+  );
 }
 
 export default Episodes;
