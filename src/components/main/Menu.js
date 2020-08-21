93a3701d36d62e682868f08a9535645045203123
JonasImm <jonas.imm@gmail.com>
2020-08-21 10:18:48 +0200

    Added JSS to menu and deleted menu.css

--- src/components/main/Menu.js
+++ src/components/main/Menu.js
@@ -1,6 +1,28 @@
 import React from "react";
-import "./Menu.css";
+import styled from "@emotion/styled";
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+
+  > a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    color: #35c9dd;
+    margin: 10px 0;
+  }
+
+  > a > img {
+    max-width: 40px;
+    max-height: 40px;
+    object-fit: contain;
+    margin-bottom: 5px;
+  }
+`;
 
 export default function Menu({ children }) {
-  return <div className="menu">{children}</div>;
+  return <Container>{children}</Container>;
 }
